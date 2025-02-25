1f69dd89c696d3c4995e25056da0d557e3184601
Tim Ottinger <tottinge@industriallogic.com>
2025-02-25 14:36:29 +0000

    DOC: revised readme

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from typing import Literal
+TestStatus = Literal["Pass","Fail","Disable","Abort","Unrun"]
+
+
 def main():
     print("Hello from testcollatorkata!")
+
+
 
 
 if __name__ == "__main__":
