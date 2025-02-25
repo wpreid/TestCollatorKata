39afa07fabebe09884c5d281a8e6ab0997d40f5d
Tim Ottinger <tottinge@industriallogic.com>
2025-02-25 14:47:24 +0000

    feat: endRun with no tests returns empty list

--- test_collator.py
+++ test_collator.py
@@ -1,9 +1,20 @@
 import unittest
+from typing import List
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertTrue(True)  # add assertion here
+class TestResult:
+    pass
+
+
+class TestCollator:
+    def endrun(self) -> List[TestResult]:
+        return []
+
+
+class CollatorTest(unittest.TestCase):
+    def test_endrun_with_no_tests_returns_empty(self):
+        result: List[TestResult] = TestCollator().endrun()
+        self.assertSequenceEqual([], result)  # add assertion here
 
 
 if __name__ == '__main__':
