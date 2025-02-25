6e51f84950a421b784a7f79a6188ac6c50960df8
Tim Ottinger <tottinge@industriallogic.com>
2025-02-25 15:02:28 +0000

    feat: ended with failing test so we know where we left off

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from typing import Literal
-TestStatus = Literal["Pass","Fail","Disable","Abort","Unrun"]
 
 
 def main():
--- test_collator.py
+++ test_collator.py
@@ -1,20 +1,42 @@
 import unittest
-from typing import List
+from typing import List, Literal
+
+TestStatus = Literal["Pass","Fail","Disable","Abort","Unrun"]
 
 
 class TestResult:
+    def __init__(self):
+        self.name = None
+
     pass
 
 
 class TestCollator:
+    def __init__(self):
+        self.result_list = []
+
     def endrun(self) -> List[TestResult]:
-        return []
+        return self.result_list
+
+    def add(self, name: str, status: TestStatus):
+        new_result = TestResult()
+        self.result_list.append(new_result)
 
 
 class CollatorTest(unittest.TestCase):
     def test_endrun_with_no_tests_returns_empty(self):
         collator: TestCollator = TestCollator()
-        self.assertSequenceEqual([], collator.endrun())  # add assertion here
+        self.assertSequenceEqual([], collator.endrun())
+
+    def test_endrun_with_one_run_of_one_test(self):
+        collator: TestCollator = TestCollator()
+        collator.add("only test", "Pass")
+        [result] = collator.endrun()
+        # assert name matches
+        self.assertEqual(result.name,"only test")
+        # assert status matches
+        # assesrt isNew is true
+
 
 
 if __name__ == '__main__':
